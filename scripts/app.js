596ceb36609cbd8ca78888cb8efcc289e6d7f29c
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-03-24 09:22:18 -0700

    holy shit I made it work

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,24 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-var $quakesList;
 var template;
 var map;
 
 
 $(document).on("ready", function() {
-
   var source = $('#earthquake-template').html();
   template = Handlebars.compile(source);
 
-getEarthquakes();
-initMap();
-
+  initMap();
+  getEarthquakes();
 });
+
+function initMap() {
+  var myLatLng = {lat: 37.78, lng: -122.44};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 2
+  });
+}
 
 function getEarthquakes() {
   $.ajax({
@@ -28,26 +33,16 @@ function onSuccess(json) {
   var earthquakeTitle = template({ objects: json.features });
   $('#earthquake-list').append(earthquakeTitle);
   json.features.forEach( function (object) {
-    console.log(object.geometry.coordinates[1]);
+    new google.maps.Marker({
+      position: {
+        lat: object.geometry.coordinates[0],
+        lng: object.geometry.coordinates[1]
+      },
+      map: map
+    });
   });
 }
-
 
 function onError() {
   console.log("error");
-}
-
-var map;
-
-function initMap() {
-  var myLatLng = {lat: 37.78, lng: -122.44};
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 9
-  });
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map
-      });
-  $('#map').append(map);
 }
